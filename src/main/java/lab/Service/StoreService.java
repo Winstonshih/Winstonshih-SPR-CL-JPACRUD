d201c81637149fb46736d04f1696a0851af5fc14
Winstonshih <Winstonshih@github.com>
2026-09-13 01:27:22 +0000

    Test run: 5/5 passed, 0 failed, 0 skipped - 2026-09-13T01:27:22.685Z

--- src/main/java/lab/Service/StoreService.java
+++ src/main/java/lab/Service/StoreService.java
@@ -2,9 +2,11 @@ package lab.Service;
 
 import lab.Model.Store;
 import lab.Repository.StoreRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
+import java.util.Optional;
 
 public class StoreService {
     StoreRepository storeRepository;
@@ -18,14 +20,14 @@ public class StoreService {
      * @return the persisted store
      */
     public Store persistStore(Store store){
-        return null;
+        return storeRepository.save(store);
     }
     /**
      * TODO: get all store entities
      * @return all store entities
      */
     public List<Store> getAllStores(){
-        return null;
+        return storeRepository.findAll();
     }
     /**
      * TODO: given an id of a store, return the store.
@@ -34,13 +36,18 @@ public class StoreService {
      * @return a store entity
      */
     public Store getStoreById(long id){
-        return null;
+        Optional<Store> optionalStore = storeRepository.findById(id);
+        if(optionalStore.isPresent()){
+            return optionalStore.get();
+        }else{
+            return null;
+        }
     }
     /**
      * TODO: given an id of an existing store, delete the store
      */
     public void deleteStore(long id){
-//        code here
+        storeRepository.deleteById(id);
     }
     /**
      * TODO: given an id and some replacement data for a store, overwrite the data of an existing store,
@@ -48,6 +55,12 @@ public class StoreService {
      * @return the updated store entity
      */
     public Store updateStore(long id, Store replacement){
+        Optional<Store> optionalStore= storeRepository.findById(id);
+        if(optionalStore.isPresent()){
+            Store store= optionalStore.get();
+            store.setName(replacement.getName());
+            return storeRepository.save(store);
+        }
         return null;
     }
 
